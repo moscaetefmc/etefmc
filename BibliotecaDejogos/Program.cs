77973c7f3a91d403ac8a90b7fcefefea149ef372
moscaetefmc <111377014+moscaetefmc@users.noreply.github.com>
2022-08-16 16:08:34 -0300

    Add files via upload

---
+++ BibliotecaDejogos/Program.cs
@@ -0,0 +1,104 @@
+﻿using BibliotecaDejogos.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDejogos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Jogo> listaDeJogos = new List<Jogo>();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            while (true)
+            {
+
+                Console.WriteLine("======BIBLIOTECA DE JOGOS======");
+
+                Console.WriteLine("1 - Adicionar Jogo");
+                Console.WriteLine("2 - Listar Jogos");
+                Console.WriteLine("3 - Editar Jogo");
+                Console.WriteLine("4 - Remover Jogo");
+                Console.Write("Opcao: ");
+
+                bool resultado;
+                String opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        resultado = AdicionarJogo(listaDeJogos);
+                        if (resultado == true)
+                        {
+                            Console.WriteLine("Cadastrado com sucesso!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Erro no cadastro.");
+                        }
+                        break;
+
+                    case "2":
+                        resultado = ListarJogos(listaDeJogos);
+                        if (resultado == true)
+                        {
+                            Console.WriteLine("Listado com sucesso!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Erro na listagem.");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção Inválida!");
+                        break;
+                }
+
+                Console.WriteLine("Pressione qualquer tecla para continuar...");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        public static bool AdicionarJogo(List<Jogo> listaDejogos)
+        {
+            Console.Clear();
+            Console.Write("Titulo: ");
+            String titulo = Console.ReadLine();
+            Console.Write("Ano: ");
+            int ano = Convert.ToInt32(Console.ReadLine());
+
+            if (titulo == "") return false;
+            if (ano < 1950) return false;
+
+            Jogo novoJogo = new Jogo(titulo, ano, "teste", 1);
+
+            listaDejogos.Add(novoJogo);
+            Console.Beep();
+
+            return true;
+        }
+
+        public static bool ListarJogos (List<Jogo> listaJogos)
+        {
+            foreach(Jogo jogo in listaJogos)
+            {
+                Console.Write("Titulo: ");
+                Console.WriteLine(jogo.getTitulo());
+                Console.Write("Ano: ");
+                Console.WriteLine(jogo.getAno());
+                Console.WriteLine("==========");
+                Console.WriteLine("");
+            }
+
+            return true;
+        }
+
+
+    }
+}
